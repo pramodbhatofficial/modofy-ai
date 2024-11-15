f913e23d872eb69f9a3dd73c9d37a6b949ea41ba
Pramod Bhat <pramodbhatofficial@gmail.com>
2024-11-16 02:48:24 +0530

    Fix build configuration and add public directory

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
+  output: 'standalone',
+  distDir: '.next',
+  images: {
+    domains: ['localhost'],
+  },
 }
 
 module.exports = nextConfig
